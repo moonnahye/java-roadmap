fc94370572ff5fbefcb87f11ca00a1dadd397212
nahye <nnhh.shine@gmail.com>
2024-11-17 23:26:20 +0900

    [basic] 섹션6. 패키지

---
+++ java-basic/src/pack/PakageMain3.java
@@ -0,0 +1,10 @@
+package pack;
+
+import pack.a.User;
+
+public class PakageMain3 {
+    public static void main(String[] args) {
+        User userA = new User();
+        pack.b.User userB = new pack.b.User();
+    }
+}
