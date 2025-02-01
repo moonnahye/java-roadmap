b108e6300a4ed02e263007dbf4f6c0a51cd8de4d
nahye <nnhh.shine@gmail.com>
2025-02-01 22:15:33 +0900

    [adv1] 섹션 4.스레드 제어와 생명주기1
    join()

---
+++ java-adv1/src/thread/control/CheckedExceptionMain.java
@@ -0,0 +1,18 @@
+package thread.control;
+
+import util.ThreadUtils;
+
+import static util.ThreadUtils.*;
+
+public class CheckedExceptionMain {
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
+    
+    static class CheckedRunnable implements Runnable {
+        @Override
+        public void run() /*throws Exception*/{ // 주석 풀면 예외 발생
+            // throw new Exception(); // 주석 풀면 예외 발생
+        }
+    }
+}
